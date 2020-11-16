f983bd5bf40a9601627aebff063bb6f256d077ef
Shaheer-rossoneri14 <69858310+Shaheer-rossoneri14@users.noreply.github.com>
2020-11-16 11:38:22 +0530

    Update settings.py

--- cmrit_quizz_app/settings.py
+++ cmrit_quizz_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r*hnduu89+t-kf52roac$z4*ypj0et5c+p9=7&i3f-uj3$_xb-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
